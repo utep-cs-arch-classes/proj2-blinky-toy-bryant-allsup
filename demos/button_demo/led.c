716d6c22b4cb6657f55952198813d6100cdf1273
bryant-allsup <baallsup@miners.utep.edu>
2021-04-07 01:14:23 -0600

    project and some notes

--- demos/button_demo/led.c
+++ demos/button_demo/led.c
@@ -12,10 +12,14 @@ void led_init()
 void led_update(){
   if (switch_state_changed) {
     char ledFlags = 0; /* by default, no LEDs on */
+    
+    //       or      (1/0)            Translation to bellow:led flags gets green or red
+    
+    ledFlags |= switch_state_down ? LED_GREEN : 0; //true or false is true green else false
+    ledFlags |= switch_state_down ? 0 : LED_RED; // if false red
 
-    ledFlags |= switch_state_down ? LED_GREEN : 0;
-    ledFlags |= switch_state_down ? 0 : LED_RED;
-
+    //       subtracts ones from leds in order to show 
+    
     P1OUT &= (0xff - LEDS) | ledFlags; // clear bits for off leds
     P1OUT |= ledFlags;         // set bits for on leds
   }
